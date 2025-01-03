06fd752d6506a7d28b43eb051b49b649267090e9
Sumit Jangir <jangirsumit1011@gmail.com>
2025-01-04 03:28:13 +0530

    add booking form route

---
+++ frontend/src/app/[id]/page.tsx
@@ -0,0 +1,261 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { usePathname, useRouter } from "next/navigation";
+import { useRestaurantContext } from "@/Context/RestaurantCotext";
+import BookingSummary from "@/Components/BookingSummary";
+
+interface Restaurant {
+  _id: string;
+  name: string;
+  location: string;
+  phone: string;
+  email: string;
+}
+interface RestaurantContext {
+  selectedRestaurant: Restaurant | null;
+}
+
+interface Slot {
+  dateAndTime: string;
+}
+
+const Booking = () => {
+  const [selectedDateTime, setSelectedDateTime] = useState<Date | null>(null);
+  const [selectedTime, setSelectedTime] = useState<Date | null>(null); // Separate state for time
+  const [slots, setSlots] = useState<Date[]>([]);
+  const [isSummaryOpen, setIsSummaryOpen] = useState<boolean>(false)
+  const [restaurantDetails, setRestaurantDetails] = useState<any>(null);
+  const [userDetails, setUserDetails] = useState<any>(null);
+  const [formData, setFormData] = useState({
+    name: "",
+    contact: "",
+    numberOfGuests: 2,
+  });
+
+  const router = useRouter()
+
+  const { selectedRestaurant } = useRestaurantContext();
+
+  console.log(selectedRestaurant?.name);
+
+  useEffect(() => {
+    if (!selectedRestaurant?.name) {
+      router.push('/');
+    }
+  }, [selectedRestaurant, router]); 
+
+
+  const restaurantId = usePathname().split("/")[1];
+
+  const handleDateChange = (date: Date | null) => {
+    if (date) {
+      setSelectedDateTime(new Date(date));
+      if (selectedTime) {
+        setSelectedDateTime(
+          new Date(
+            date.setHours(selectedTime.getHours(), selectedTime.getMinutes())
+          )
+        );
+      }
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const areDatesEqual = (date1: Date, date2: Date) => {
+    return (
+      date1.getFullYear() === date2.getFullYear() &&
+      date1.getMonth() === date2.getMonth() &&
+      date1.getDate() === date2.getDate() &&
+      date1.getHours() === date2.getHours() &&
+      date1.getMinutes() === date2.getMinutes()
+    );
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!selectedDateTime) {
+      toast.error("Please fill in all fields.");
+      return;
+    }
+    const checkSlot = slots.some((slot) =>
+      areDatesEqual(slot, selectedDateTime)
+    );
+    if (checkSlot) {
+      toast.error(
+        "Time slot is already taken. Please choose a different time."
+      );
+      return;
+    }
+
+    try {
+      const bookingData = {
+        ...formData,
+        dateAndTime: selectedDateTime,
+        restaurantId,
+      };
+
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/booking`,
+        bookingData
+      );
+      if (response.status === 201) {
+        // toast.success(response.data.message);
+        setRestaurantDetails(selectedRestaurant);
+        setIsSummaryOpen(true)
+        setUserDetails(response.data.booking)
+        
+        setFormData({
+          name: "",
+          contact: "",
+          numberOfGuests: 2,
+        });
+        setSelectedDateTime(null);
+      } else {
+        toast.error("Failed to create booking: " + response.data.message);
+      }
+    } catch (error) {
+      alert("Error creating booking");
+      console.error(error);
+    }
+  };
+
+  const minTime = new Date();
+  minTime.setHours(10, 0, 0, 0);
+
+  const maxTime = new Date();
+  maxTime.setHours(23, 0, 0, 0);
+
+  const checkAvailableSlots = async (): Promise<void> => {
+    try {
+      const response = await axios.get<Slot[]>(
+        `${process.env.NEXT_PUBLIC_API_URL}/booking/slots/${restaurantId}`
+      );
+
+      if (response.status === 200) {
+        const bookedSlots = response.data;
+        setSlots(bookedSlots.map((item) => new Date(item.dateAndTime)));
+      } else {
+        alert("Failed to fetch booking slots: " + response.data);
+      }
+    } catch (error) {
+      alert("Error fetching booking slots");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    checkAvailableSlots();
+  }, [selectedDateTime]);
+
+
+  return (
+    <div className="flex items-center h-screen justify-center bg-gray-200">
+      <div className="relative w-full sm:w-[600px] bg-gray-200  border-2 border-zinc-500 rounded-lg shadow-lg p-4 sm:p-6 mx-[10px] sm:mx-0">
+        <div className="flex justify-center mb-3">
+          <h2 className="text-3xl font-bold">Booking Form</h2>
+        </div>
+
+        {isSummaryOpen && <BookingSummary restaurantDetails={restaurantDetails} setIsSummaryOpen={setIsSummaryOpen} userDetails={userDetails} />
+    }
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <h2 className="text-lg">
+            {selectedRestaurant?.name
+              ? `Restaurant Name: ${selectedRestaurant.name}`
+              : ""}
+          </h2>
+          <div>
+            <label className="block">
+              Name:
+              <input
+                type="text"
+                name="name"
+                placeholder="Enter name"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+                className="mt-1 w-full p-2 rounded bg-gray-300"
+              />
+            </label>
+          </div>
+
+          <div>
+            <label className="block">
+              Contact:
+              <input
+                type="text"
+                name="contact"
+                placeholder="Enter Contact number"
+                value={formData.contact}
+                onChange={handleInputChange}
+                required
+                className="mt-1 w-full p-2 rounded bg-gray-300"
+              />
+            </label>
+          </div>
+
+          <div className="flex flex-col">
+            Date and Time:
+            <DatePicker
+              selected={selectedDateTime}
+              onChange={handleDateChange}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={30}
+              dateFormat="dd/MM/yyyy HH:mm"
+              className="mt-1 w-full sm:w-28  p-2 rounded bg-gray-300"
+              required
+              minTime={minTime}
+              maxTime={maxTime}
+              excludeTimes={slots.filter(
+                (slot) =>
+                  slot.getDate() === selectedDateTime?.getDate() &&
+                  slot.getMonth() === selectedDateTime?.getMonth() &&
+                  slot.getFullYear() === selectedDateTime?.getFullYear()
+              )}
+              onChangeRaw={(e) => {}}
+            />
+          </div>
+
+          <div>
+            <label className="block">
+              Number of Guests:
+              <input
+                type="number"
+                name="numberOfGuests"
+                value={formData.numberOfGuests}
+                onChange={handleInputChange}
+                min={1}
+                max={20}
+                required
+                className="mt-1 w-full p-2 rounded bg-gray-300"
+              />
+            </label>
+          </div>
+
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="my-2 py-2 px-4 bg-green-500 text-white rounded hover:bg-green-600"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Booking;
