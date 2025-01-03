7b15a6a8f472d7fc6b702e3caf1f5ead316250b5
Sumit Jangir <jangirsumit1011@gmail.com>
2025-01-03 22:00:25 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const BookingRoute = require("./Routes/BookindRoute");
-const RestaurantRoute = require("./Routes/RestaurantRoute");
+const BookingRoute = require("./Routes/BookindRoute")
+const RestaurantRoute = require("./Routes/RestaurantRoute")
+// const RestaurantModel = require("./Model/RestaurantModel");
+// const BookingModel = require("./Model/BookingModel");
 
 require("./config/db");
 require("dotenv").config();
@@ -12,11 +14,17 @@ require("dotenv").config();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/booking", BookingRoute);
-app.use("/restaurent", RestaurantRoute);
+app.use('/booking', BookingRoute);
+app.use('/restaurent', RestaurantRoute);
+
+
+
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Vercel!");
+    res.send("Hello, Vercel!");
 });
 
-module.exports = app;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
