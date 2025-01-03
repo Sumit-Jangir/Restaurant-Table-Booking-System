89457850eaa20d07f3479de70518e0d2c57a792e
Sumit Jangir <jangirsumit1011@gmail.com>
2025-01-03 21:39:11 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const BookingRoute = require("./Routes/BookindRoute")
-const RestaurantRoute = require("./Routes/RestaurantRoute")
-// const RestaurantModel = require("./Model/RestaurantModel");
-// const BookingModel = require("./Model/BookingModel");
+const BookingRoute = require("./Routes/BookindRoute");
+const RestaurantRoute = require("./Routes/RestaurantRoute");
 
 require("./config/db");
 require("dotenv").config();
@@ -14,17 +12,11 @@ require("dotenv").config();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use('/booking', BookingRoute);
-app.use('/restaurent', RestaurantRoute);
-
-
-
-const PORT = process.env.PORT || 7070;
+app.use("/booking", BookingRoute);
+app.use("/restaurent", RestaurantRoute);
 
 app.get("/", (req, res) => {
-    res.send("Hello, Vercel!");
+  res.send("Hello, Vercel!");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
